5789886e454be9348e55ef2e3be9a2a8a05fc6ea
NakamasCode <92761955+NakamasCode@users.noreply.github.com>
2026-03-24 10:10:42 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
+
+    cloudinary.config(
+        cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+        api_key=os.environ.get("CLOUDINARY_API_KEY"),
+        api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+        secure=True
+    )
     
     db.init_app(app)
     
@@ -17,9 +24,4 @@ def create_app():
         
     return app
 
-cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    secure=True
-)
+
